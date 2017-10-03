1ede81ed80fc253bf25d5ad38eaafcf49d7bbe83
Felix <31891905+felixtomlinson@users.noreply.github.com>
2017-10-03 14:37:12 +0100

    Update Age-in-days-generator.py

--- Python_Issue/Age-in-days-generator.py
+++ Python_Issue/Age-in-days-generator.py
@@ -18,11 +18,12 @@ def daysBetweenDates(y1, m1, d1, y2, m2, d2):
     
     days_y = 0
     while (y2-1)>y1:
-        if isleapyear(y2)==False:
+        if year_is_leap(y2)==False:
             days_y=days_y+365
         else:
             days_y=days_y+366
         y2=y2-1
+        
     days_m = 0
     if y2>y1:
         for e in daysofmonths[:(m2-2)] + daysofmonths[(m1-1):]:
