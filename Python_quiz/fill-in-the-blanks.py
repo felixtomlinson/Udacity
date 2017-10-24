d0a69565613ab4f5f6ae6358ea7e296d33e977c8
Felix <31891905+felixtomlinson@users.noreply.github.com>
2017-10-24 17:53:14 +0100

    made udacity required changes
    These included: changing apostrophes to normal ones, making all inputs lowercase and making sure the answers corresponded, adding extra space between all functions and inputs, and naming the variables in my for loops to reflect what they were better.

--- Python_quiz/fill-in-the-blanks.py
+++ Python_quiz/fill-in-the-blanks.py
@@ -1,22 +1,26 @@
 easyquestion = "___1___ all my ___2___ seemed so far away. \
 Now it looks as though they’re here to stay. \
 ___3___, I believe in ___1___. \
-___4___ I’m not half the man I used to be. \
-There’s a shadow hanging over me. \
+___4___ I'm not half the man I used to be. \
+There's a shadow hanging over me. \
 ___3___, ___1___ came ___4___."
 
-easyanswers = ['Yesterday', 'troubles', 'Oh','Suddenly']
 
-mediumquestion = "I’ve got ___1___ on a ___2___ day \
-When it’s ___3___ outside I’ve got the month of May \
-Well I ___4___ you’d say \
+easyanswers = ['yesterday', 'troubles', 'oh','suddenly']
+
+
+mediumquestion = "I've got ___1___ on a ___2___ day \
+When it's ___3___ outside I’ve got the month of May \
+Well I ___4___ you'd say \
 What can ___5___ me feel this way? \
 ___6___ (___6___, ___6___) \
-Talkin’ ’bout ___6___ (___6___)"
+Talkin' 'bout ___6___ (___6___)"
 
-mediumanswers = ['sunshine', 'cloudy', 'cold', 'guess', 'make', 'My girl']
 
-hardquestion = "Go ’way from my window \
+mediumanswers = ['sunshine', 'cloudy', 'cold', 'guess', 'make', 'my girl']
+
+
+hardquestion = "Go 'way from my window \
 Leave at your own chosen speed \
 ___1___ not the one ___2___ want, ___3___ \
 ___1___ not the one ___2___ need \
@@ -29,11 +33,15 @@ But it ___7___ me, ___3___ \
 No, no, no, it ___7___ me, ___3___ \
 It ___7___ me ___4___ ___5___ for, ___3___"
 
-hardanswers = ["I'm", 'you', 'babe', "you're", "lookin'",'someone',"ain't"]
+
+hardanswers = ["i'm", 'you', 'babe', "you're", "lookin'",'someone',"ain't"]
+
 
 answer_areas = ['___1___', '___2___', '___3___', '___4___', '___5___', '___6___', '___7___', '___8___', '___9___', '___10___']
 
-easy_medium_hard = [input("What degree of difficulty do you want to play: easy, medium or hard?")]
+
+easy_medium_hard = [input("What degree of difficulty do you want to play: easy, medium or hard?").lower()]
+
 
 def easy_medium_hard_asker():
   ''' This function uses the input from that the person deciding on \
@@ -41,14 +49,15 @@ def easy_medium_hard_asker():
   right questions. If they do not provide one of the three on offer it asks \
   them again until they do.''' 
   difficulty = easy_medium_hard
-  for i in difficulty:
-    if i == 'easy':
+  for degree in difficulty:
+    if degree == 'easy':
       return ('\n'+ 'Question:' +'\n'*2 + easyquestion +'\n')
-    if i == 'medium':
+    if degree == 'medium':
       return ('\n'+ 'Question:' +'\n'*2 + mediumquestion +'\n')
-    if i == 'hard':
+    if degree == 'hard':
       return ('\n'+ 'Question:' +'\n'*2 + hardquestion +'\n')
     difficulty.append(input('\n'+ "Sorry, you must select easy, medium or hard. Please try again:"))
+
 
 def answer_selector(answer):
   ''' This function allows you to input one of the three \
@@ -59,7 +68,8 @@ def answer_selector(answer):
     return (mediumanswers)
   if answer == 'hard':
     return (hardanswers)
-    
+
+
 def question_selector(answer):
   ''' This function allows you to input one of the three \
   degrees of difficulty and return the questions that correspond to that degree'''
@@ -70,6 +80,7 @@ def question_selector(answer):
   if answer == 'hard':
     return (hardquestion)
 
+
 def correct_or_not(word, answer_list, index_number):
   ''' This function lets you know if the word that is being inputted \
   corresponds to the answer at the index of the index number'''
@@ -77,6 +88,7 @@ def correct_or_not(word, answer_list, index_number):
     return True
   else:
     return False
+
 
 def text_accumulator(Question, parts_of_speech, Answers, index_number):
     ''' This function works out which words need to be replaced \
@@ -94,6 +106,7 @@ def text_accumulator(Question, parts_of_speech, Answers, index_number):
     replaced = " ".join(replaced)
     return replaced
 
+
 def prompter(question, word, answers, index_number, answer_areas):
   ''' This text propts people to either answer the next question, or \
   provide a different answer to the same question depending on whether or \
@@ -101,13 +114,14 @@ def prompter(question, word, answers, index_number, answer_areas):
   text with answers filled in'''
   variable = correct_or_not(word, answers, index_number)
   while not variable:
-    result = input('\n'+ "Sorry that's wrong, why don't you try again?")
+    result = input('\n'+ "Sorry that's wrong, why don't you try again?").lower()
     if correct_or_not(result, answers, index_number):
       updated_text = text_accumulator(question, answer_areas, answers, index_number)
       return ('\n'+ updated_text +'\n'*2+"Great! Now try the next one"+'\n')
   if variable:
     updated_text = text_accumulator(question, answer_areas, answers, index_number)
     return ('\n'+ updated_text + '\n'*2 +"Great! Now let's try the next one"+'\n')
+
 
 def final_question(question, word,answers, index_number, answer_areas):
   ''' This function propts people either to answer the question again or \
@@ -116,7 +130,7 @@ def final_question(question, word,answers, index_number, answer_areas):
   text with answers filled in'''
   variable = correct_or_not(word, answers, index_number)
   while not variable:
-    result = input('\n'+ "Sorry that's wrong, why don't you try again?")
+    result = input('\n'+ "Sorry that's wrong, why don't you try again?").lower()
     if correct_or_not(result, answers, index_number):
       updated_text = text_accumulator(question, answer_areas, answers, index_number)
       return ('\n'+ updated_text + '\n'*2 +"Congratulations you have completed the quiz!!!")
@@ -124,17 +138,21 @@ def final_question(question, word,answers, index_number, answer_areas):
     updated_text = text_accumulator(question, answer_areas, answers, index_number)
     print ('\n'+ updated_text +'\n'*2 + "Congratulations you have completed the quiz!!!")
 
+
 def question_asker(answer_areas):
   ''' This function asks people the questions in sequence'''
   answers = answer_selector(easy_medium_hard[-1])
   question = question_selector(easy_medium_hard[-1])
-  for i in range(len(answers)):
-    quiz_answer = input("What word fits into the sentence in place of " + answer_areas[i] + "?")
-    if i < (len(answers)-1):
-      print (prompter(question, quiz_answer, answers, i, answer_areas))
-    elif i == (len(answers)-1):
-      print (final_question(question, quiz_answer,answers,i, answer_areas))
-    question = text_accumulator(question, answer_areas, answers, i)
+  for answer_index in range(len(answers)):
+    quiz_answer = input("What word fits into the sentence in place of " + answer_areas[answer_index] + "?").lower()
+    if answer_index < (len(answers)-1):
+      print (prompter(question, quiz_answer, answers, answer_index, answer_areas))
+    elif answer_index == (len(answers)-1):
+      print (final_question(question, quiz_answer,answers, answer_index, answer_areas))
+    question = text_accumulator(question, answer_areas, answers, answer_index)
+
 
 print (easy_medium_hard_asker()) 
+
+
 print (question_asker(answer_areas))
